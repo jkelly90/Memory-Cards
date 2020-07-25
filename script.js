d3a83d0e06ae849fd1b1de183af4ead8dd740b56
jkelly90 <jlfkelly@gmail.com>
2020-07-25 18:24:40 +0200

    added variables

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-
-* {
-    box- sizing: border - box;
-}
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
