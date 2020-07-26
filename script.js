5861286ce873beac32cedbc03c3468aedede4947
jkelly90 <jlfkelly@gmail.com>
2020-07-26 11:57:06 +0200

    added cards data

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+
+
+// Keep track of current card
+let currentActiveCard = 0;
+
+// Store DOM cards
+const cardsEl = [];
+
+// Store card data
+const cardsData = getCardsData();
+
+const cardsData = [
+    {
+        question: 'What must a variable begin with?',
+        answer: 'A letter, $ or _'
+    }, {
+        question: 'What is a variable?',
+        answer: 'Container for a piece of data'
+    },
+    {
+        question: 'Example of Case Sensitive Variable',
+        answer: 'thisIsAVariable'
+    }
+];
